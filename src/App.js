607f78645605b3fff537615a7ba868029336fe26
DannyDenver <djt09d@fulbrightmail.org>
2020-02-19 19:19:13 -0700

    use params and state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
-class App extends Component {
-  render() {
-    return (
+const app = props => {
+  const [ personsState, setPersonsState ] = useState({
+    persons: [
+      { name: 'danny', age: 28 },
+      { name: "CJ", age: 30 },
+      { name: "Stephanie", age: 27 }
+    ]
+  });
+
+  // can use multiple useStates; useState hook
+  const [ zodiacSigns, zodiacSignsState ] = useState('gemini');
+
+  const switchNameHandler = () => {
+    let clonedPeople = [...personsState.persons];
+    let currentIndex = clonedPeople.length;
+    console.log('howdy')
+
+    while (0 !== currentIndex) {
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      let temporaryValue = clonedPeople[currentIndex];
+      clonedPeople[currentIndex] = clonedPeople[randomIndex];
+      clonedPeople[randomIndex] = temporaryValue;
+    }
+
+    // does not merge; replaces old state; must add other properties
+    setPersonsState({persons: [...clonedPeople]});
+  }
+
+  return(
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -13,12 +42,15 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <h1>I'm a react app.</h1>
+        <button onClick={switchNameHandler}>Switch Name</button>
+
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age} >I like to swim. </Person>
+
+        {zodiacSigns}
       </div>
-    );
+  );
 
-    //eturn React.createElement('div', { className: 'App' }, React.createElement('h1', null, "howdy partner!"));
   }
-}
 
-export default App;
+export default app;
